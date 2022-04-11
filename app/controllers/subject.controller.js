916f499d35404260a60db0e74350c2c8bade290e
alaa.assali1996 <alaa.assali1996@gmail.com>
2022-04-11 02:14:19 +0100

    add subject name unique

--- app/controllers/subject.controller.js
+++ app/controllers/subject.controller.js
@@ -2,21 +2,30 @@ const config = require("../config/auth.config");
 const db = require("../models");
 const Subject = db.subject;
 
-exports.createSubject = (req, res) => {
-  const subject = new Subject({
-    name: req.body.name,
-    nbHeures: req.body.nbHeures,
-    ects: req.body.ects,
+exports.createSubject = async (req, res) => {
 
-  });
+  const fetchedSubject = await Subject.findOne({name: req.body.name},
+    (err, subject) => {
+      if(subject) {
+        return res.send({ message: "Subject already exists" });
+      } else {
+        const subject = new Subject({
+          name: req.body.name,
+          nbHeures: req.body.nbHeures,
+          ects: req.body.ects,
+      
+        });
+      
+        subject.save((err, subject) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+          res.send({ message: "Subject was registered successfully!" });
+        });
+      }
+    });
 
-  subject.save((err, subject) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-    res.send({ message: "Subject was registered successfully!" });
-  });
 };
 
 exports.getSubjectById = (req, res) => {
